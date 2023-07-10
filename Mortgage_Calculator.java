24a221da855924b633cff5b475263893fe678a3a
Collincg <128854401+Collincg@users.noreply.github.com>
2023-07-10 17:07:49 -0500

    Update Mortgage_Calculator.java

--- Mortgage_Calculator.java
+++ Mortgage_Calculator.java
@@ -34,10 +34,10 @@ public class Mortgage_Calculator {
 
     public static double readNumber(String prompt, double min, double max){
         Scanner scanner = new Scanner(System.in);
-        double value; // make 'value' a double so that it can be changed to any type later one when the function is called
+        double value;
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextFloat(); // principal
+            value = scanner.nextFloat();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a number between" + min + "and" + max);
